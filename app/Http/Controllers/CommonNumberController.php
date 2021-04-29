da82df4916e0f9cca40e87450de2759184436b04
mounitbhandari <tataibhandari@gmail.com>
2021-04-29 20:29:27 +0530

    working on common numbers

---
+++ app/Http/Controllers/CommonNumberController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CommonNumber;
+use Illuminate\Http\Request;
+
+class CommonNumberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\CommonNumber  $commonNumber
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CommonNumber $commonNumber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\CommonNumber  $commonNumber
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CommonNumber $commonNumber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\CommonNumber  $commonNumber
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CommonNumber $commonNumber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\CommonNumber  $commonNumber
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CommonNumber $commonNumber)
+    {
+        //
+    }
+}
