585bbfee6366400d0bd3906ce8ae5422db33293f
Priyam Ghosh <priyamghosh73@gmail.com>
2021-05-06 17:24:59 +0530

    validation added in result

--- app/Http/Controllers/ManualResultDigitController.php
+++ app/Http/Controllers/ManualResultDigitController.php
@@ -23,6 +23,31 @@ class ManualResultDigitController extends Controller
         $gameDate = $currentDate = Carbon::now()->format('Y-m-d');
         $result = $requestedData->master['aandar'];
 
+        $ManualResultDigitData = ManualResultDigit::where('result',$result)->where('game_date',$currentDate)->first();
+
+        $fr_value = DB::select('select game_date, fr_value from (select result_masters.game_date,if(result_masters.draw_master_id=1,(result_details.result_row*10 + result_details.result_col),0) as fr_value from result_masters
+            inner join result_details on result_details.result_master_id = result_masters.id
+             where result_masters.draw_master_id = 1 and result_masters.game_date>= ? and result_masters.game_date<= ? order by result_masters.game_date desc
+             ) as table1
+             where fr_value=?', [$currentDate,$currentDate,$result]);
+
+        $sr_value = DB::select('select game_date, sr_value from (select result_masters.game_date,if(result_masters.draw_master_id=2,(result_details.result_row*10 + result_details.result_col),0) as sr_value from result_masters
+            inner join result_details on result_details.result_master_id = result_masters.id
+             where result_masters.draw_master_id = 2 and result_masters.game_date>= ? and result_masters.game_date<= ? order by result_masters.game_date desc
+             ) as table1
+             where sr_value=?', [$currentDate,$currentDate,$result]);
+
+//        return response()->json(array('success' => $sr_value, 'message' => $fr_value),200);
+
+        if($ManualResultDigitData){
+            return response()->json(array('success' => 1, 'message' => 'Already Exist'),200);
+        }
+
+        if($fr_value && $sr_value){
+            return response()->json(array('success' => 1, 'message' => 'Already Exist'),200);
+        }
+
+//        return response()->json(array('success' => 0, 'message' => $ManualResultDigitData),200);
         try
         {
             $manualResultDigit = new ManualResultDigit();
